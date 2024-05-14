15bb632878a4900952818655222bc926a982e7a9
d.zubakhin <dmitryzubakhin@mail.ru>
2024-05-14 15:59:37 +0300

    Parser: Complete Json parser
    -Complete types:
    *Register(name, address, desc, size, mode, options)
    *Option(name, desc, bit range, format)
    
    -Attach reg description example

--- parser/register/option.hpp
+++ parser/register/option.hpp
@@ -11,17 +11,15 @@ namespace Parser {
 
     struct Option {
         std::string name;
-        std::string desc;
+        std::string description;
         std::pair<uint64_t, uint64_t> bit_range;
-        RWMode rw_mode;
         Format format;
 
         void dump() const {
             std::cout << "Option:" << "\n"
                 << "\t\t" << "name: " << name << "\n"
-                << "\t\t" << "desc: " << desc << "\n"
+                << "\t\t" << "description: " << description << "\n"
                 << "\t\t" << "bit_range: [" << bit_range.first << ", " << bit_range.second << "]" << "\n"
-                << "\t\t" << "rw_mode: " << rw_mode << "\n" 
                 << "\t\t" << "format: " << format << std::endl;
         }
     };
--- parser/register/register.hpp
+++ parser/register/register.hpp
@@ -9,15 +9,19 @@ namespace Parser {
 
     struct Register {
         std::string name;
-        uint64_t offset;
+        uint64_t address;
+        std::string description;
         uint64_t size;
+        RWMode mode;
         std::vector<Option> options;
 
         void dump() const {
             std::cout << "Register:" << "\n"
                 << "\t" << "name: " << name << "\n"
-                << "\t" << "offset: " << offset << "\n"
-                << "\t" << "size: " << size << std::endl;
+                << "\t" << "address: " << address << "\n"
+                << "\t" << "description: " << description << "\n"
+                << "\t" << "size: " << size << "\n"
+                << "\t" << "RWMode: " << mode << std::endl;
 
             for (const auto& op : options)
                 op.dump();
