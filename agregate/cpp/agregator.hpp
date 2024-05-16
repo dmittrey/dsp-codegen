f8c2d3a1ab8154b9c1e354bfedb875a9acbe61e9
d.zubakhin <dmitryzubakhin@mail.ru>
2024-05-17 01:00:59 +0300

    Agregate: Cleanup

--- agregate/cpp/agregator.hpp
+++ agregate/cpp/agregator.hpp
@@ -1,63 +1,84 @@
 #pragma once
 
 #include "interface/agregator.hpp"
+#include "interface/iregparser.hpp"
+#include "interface/iserializer.hpp"
 
 #include "cpp/layout.hpp"
-#include "cpp/model.hpp"
+#include "cpp/extra.hpp"
 
-#include "cpp/type/struct.hpp"
-#include "cpp/type/void.hpp"
+#include "cpp/ioctl/function.hpp"
 
-using namespace Utility;
+using namespace Generate::Cpp;
 using namespace Generate;
+
+using Parser::IRegParser;
+
+using Utility::Register;
+using Utility::Option;
 
 namespace Agregate {
 
-    struct CppAgregator final : Agregator {
+    struct CppAgregator final : IAgregator {
+    private:
+        const std::unique_ptr<IRegParser> parser_;
+        const std::unique_ptr<ISerializer> serializer_;
+
+    public:
         CppAgregator(std::unique_ptr<IRegParser> &parser, 
-                    std::unique_ptr<ISerializer> &serializer) : Agregator(parser, serializer) {}
+                    std::unique_ptr<ISerializer> &serializer) : 
+                        parser_(std::move(parser)), serializer_(std::move(serializer)) {}
 
         void process() const override {
-            auto vec = parser_->registers();
+            auto regs_ = parser_->registers();
 
             CppLayout layout;
 
-            for (const auto& reg : vec) {
-                auto sname = CppRVal{Types::make_struct(), reg.name};
+            auto e = make_anon_enum();
+            layout.add_model(e);
 
-                auto sdecl = new CppDecl(sname);
-                layout.add_model(sdecl); // Decl before function that use sname
+            for (const auto& reg : regs_) {
+                auto s = new CppStructure{{Types::make_struct(), reg.name}};
+                layout.add_model(s);
 
-                auto s = new CppStructure{sname};
                 for (const auto& opt : reg.options) {
-                    auto opt_type = opt.type();
+                    auto opt_type = option_type(opt);
 
-                    CppRVal field = {opt_type, opt.name};
-                    s->field_add(field);
+                    CppRVal opt_field = {opt_type, opt.name};
+                    s->field_add(opt_field);
 
                     // Option modifier
                     // Later... Need to move to Base, delete all methods and use derived CppBool
-                    if (opt_type.name == "bool") {
-                        auto se = new CppFunction{{Types::make_void(), reg.name + '_' + "enable" + '_' + opt.name}};
-                        auto sd = new CppFunction{{Types::make_void(), reg.name + '_' + "disable" + '_' + opt.name}};
-                        layout.add_model(se);
-                        layout.add_model(sd);
-                    } else {
-                        auto sf = new CppFunction{{Types::make_void(), reg.name + '_' + 's' + '_' + opt.name}};
-                        sf->param_add(CppRVal{opt_type, opt.name});
-                        layout.add_model(sf);
-                    }
+                    auto sf = new IoctlCppFunction{{Types::make_void(), reg.name + '_' + "s" + '_' + opt.name}, 
+                                                    std::string("IOCTL_") + "S_" + opt.name, opt_field};
+                    e->field_add(CppRVal{Types::make_empty(), std::string("IOCTL_") + "S_" + opt.name});
+                    layout.add_model(sf);
 
                     // Option getter
-                    auto gf = new CppFunction{{opt_type, reg.name + '_' + 'g' + '_' + opt.name}};
+                    auto gf = new IoctlCppFunction{{opt_type, reg.name + '_' + "g" + '_' + opt.name},
+                                                    std::string("IOCTL_") + "G_" + opt.name};
+                    e->field_add(CppRVal{Types::make_empty(), std::string("IOCTL_") + "G_" + opt.name});
                     layout.add_model(gf);
                 }
-
-                layout.add_model(s);
             }
 
-            layout.prepare();
             serializer_->serialize(layout);
+        }
+
+    private:
+        static Type option_type(const Option &opt)
+        {
+            auto opt_size = opt.size();
+            if (opt_size == 1)
+                return Types::make_bool();
+            else if (opt_size <= 8)
+                return Types::make_uint8();
+            else if (opt_size <= 16)
+                return Types::make_uint16();
+            else if (opt_size <= 32)
+                return Types::make_uint32();
+            else
+                return Types::make_uint64();
         }
     };
 
--- agregate/interface/agregator.hpp
+++ agregate/interface/agregator.hpp
@@ -1,30 +1,10 @@
 #pragma once
 
-#include "interface/iregparser.hpp"
-#include "interface/iserializer.hpp"
-
-#include "register/register.hpp"
-
-using Parser::Register;
-using Parser::IRegParser;
-using Generate::ISerializer;
-
 namespace Agregate {
 
-    struct Agregator {
-    protected:
-        const std::unique_ptr<IRegParser> parser_;
-        const std::unique_ptr<ISerializer> serializer_;
-
-    public:
-        Agregator(std::unique_ptr<IRegParser> &parser, 
-                    std::unique_ptr<ISerializer> &serializer) :
-            parser_(std::move(parser)), serializer_(std::move(serializer)) {}
-        
-        virtual ~Agregator() {}
-
-    public:
+    struct IAgregator {
         virtual void process() const = 0;
+        virtual ~IAgregator() { };
     };
 
 }; /* Agregate */
