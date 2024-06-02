5c704dfe5dbb542ef637704fd2be8394046b9257
d.zubakhin <dmitryzubakhin@mail.ru>
2024-06-02 22:05:52 +0300

    Cleanup main

--- main.cc
+++ main.cc
@@ -5,33 +5,22 @@
 #include "json/regparser.hpp"
 
 using namespace Generate;
-using namespace Parser;
+using namespace Parse;
+using namespace Serialize;
 
 /*
 
-DRAWIO!!!
-
-// Anon structures if possible move to std::optional name of structure
-
-Подумать как нормально передавать внутрь комментарии, чтобы комментить опции(по уму внутрь field_add надо пихать Model)
-
-6) Добавить выплевывание ioctl в иной файл(доп параметр)
-
-УРА КОДГЕН ГОТОВ
-
-1) Сделать модуль генератора kernel
-
 1) Сделать маппинг наконец fp <-> fixp
 
-LATER...
-3) Добавить автогенерацию кернел модуля чтобы работать с регистрами и подставить вызов этих функций уже в код сеттеров геттеров
+2) Сделать модуль генератора kernel
+
 */
 
 int main() {
-  std::unique_ptr<IRegParser> parser = make_json_regparser("parser/regtest.json");
+  std::unique_ptr<IRegParser> parser = make_json_regparser("regtest.json");
   std::unique_ptr<ISerializer> serializer = make_file_serializer("user.outp");
   std::unique_ptr<ISerializer> serializer_ioctl = make_file_serializer("ioctl.outp");
   
-  process_userspace_ctrls(parser->registers(), serializer);
-  process_ioctl_enum(parser->registers(), serializer_ioctl);
+  generate_userspace_ctrls(parser->registers(), serializer);
+  generate_ioctl_enum(parser->registers(), serializer_ioctl);
 }
