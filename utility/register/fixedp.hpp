4be7057666bc6499ba8b45015d02b28e54144c79
d.zubakhin <dmitryzubakhin@mail.ru>
2024-06-05 21:10:19 +0300

    fixup

--- utility/register/fixedp.hpp
+++ utility/register/fixedp.hpp
@@ -9,23 +9,23 @@ namespace util {
 
     struct FixedP {
         Format format;
-        uint8_t integer_bits;
+        uint8_t int_bits;
         uint8_t fract_bits;
 
         std::string str() const {
             switch (format) {
                 case HEX_UNSIGNED:
-                    return "u" + std::to_string(integer_bits);
+                    return "u" + std::to_string(int_bits);
                 case HEX_SIGNED:
-                    return "s" + std::to_string(integer_bits);
+                    return "s" + std::to_string(int_bits);
                 case FP_UNSIGNED: /* u4q3 */
-                    return "u" + std::to_string(integer_bits) + "q" + std::to_string(fract_bits);
+                    return "u" + std::to_string(int_bits) + "q" + std::to_string(fract_bits);
                 case FP_SIGNED: /* s4q3 */
-                    return "s" + std::to_string(integer_bits) + "q" + std::to_string(fract_bits);
+                    return "s" + std::to_string(int_bits) + "q" + std::to_string(fract_bits);
                 case FP_SIGN_MAGNITUDE: /* sm4q3 */
-                    return "sm" + std::to_string(integer_bits) + "q" + std::to_string(fract_bits);
+                    return "sm" + std::to_string(int_bits) + "q" + std::to_string(fract_bits);
                 case FP_TWOS_COMPLEMENT: /* t4q3 */
-                    return "t" + std::to_string(integer_bits) + "q" + std::to_string(fract_bits);
+                    return "t" + std::to_string(int_bits) + "q" + std::to_string(fract_bits);
             }   
         }
     };
