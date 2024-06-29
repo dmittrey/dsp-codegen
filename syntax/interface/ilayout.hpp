69ea53666188c1660f692fdb46320f466fec832f
d.zubakhin <dmitryzubakhin@mail.ru>
2024-06-29 14:41:00 +0300

    syntax: interface: refactor

--- syntax/interface/ilayout.hpp
+++ syntax/interface/ilayout.hpp
@@ -6,10 +6,9 @@ namespace stx {
 
     /* Abstraction to group models */
     struct ILayout : IModel {
+    public:
         virtual const std::vector<IModel*>& models() const = 0;
         virtual ILayout& add_model(IModel* model) = 0;
-        virtual void add_header(const std::string &name) { headers_.push_back(name); }
-        virtual ~ILayout() {};
     };
 
 }; /* syntax */
--- syntax/interface/imodel.hpp
+++ syntax/interface/imodel.hpp
@@ -12,13 +12,16 @@ namespace stx {
 
     public:
         virtual const std::vector<std::string>& headers() const & { return headers_; } // Const prevent edit in consumer
-        virtual std::string code() = 0;
-        virtual ~IModel() {};
-
+        virtual void add_header(const std::string &name) { headers_.push_back(name); }
+    
     public:
+        virtual std::string code() = 0;
         operator std::string() {
             return code();
         }
+
+    public:
+        virtual ~IModel() {};
     };
 
 }; /* syntax */
